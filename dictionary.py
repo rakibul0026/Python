3fadbb1e44aae55cd73c1fbc423bc57c6a06e38d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-05 02:11:45 +0600

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+student={"name":"sifat Hassan","age":8,"courses":"python"}
+
+#empty dictionary
+empty_dictionary={}
+print(type(empty_dictionary))
+
+#empty dictionary another way
+
+empty_dictionary=dict()
+print(type(empty_dictionary))
