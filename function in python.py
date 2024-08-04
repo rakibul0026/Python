3105a563abba5478bf5732b2105dda7f9c0ddf5a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-05 02:16:43 +0600

    Create function in python.py

---
+++ function in python.py
@@ -0,0 +1,14 @@
+def display():
+    print("wellcome to Bangladesh")
+    
+student_1="ratul khan" 
+print(student_1)
+display() 
+  
+student_2="shawon" 
+print(student_2)
+display()   
+
+student_3="mehedi" 
+print(student_3)
+display()   
