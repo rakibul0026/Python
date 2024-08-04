3efd4bd0cdf34e3793518cae04dd959572455deb
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-05 02:23:54 +0600

    Update date_line.py

--- date_line.py
+++ date_line.py
@@ -1,1 +1,17 @@
 
+import datetime
+
+# Current datetime
+print(datetime.datetime.now())
+
+# Current UTC time
+print(datetime.datetime.utcnow())
+
+# Get today's date
+print(datetime.date.today())
+
+# Timestamp
+random_date = datetime.date.fromtimestamp(12345678)
+print(random_date.year)
+print(random_date.month)
+print(random_date.day)
