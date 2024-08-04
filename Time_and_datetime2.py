4eccdcf896f9f630df376cd939751f5b64a9cbb7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-05 02:23:08 +0600

    Update Time_and_datetime2.py

--- Time_and_datetime2.py
+++ Time_and_datetime2.py
@@ -1,1 +1,10 @@
+import time
 
+print(time.time())
+second=1722800105.267946
+print(time.ctime(second))
+print(time.localtime(second))
+
+print("i am new line")
+time.sleep(2)
+print("I am another line")
