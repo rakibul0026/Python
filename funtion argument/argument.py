29474a53561e3dc2d07486a0084adc7a6c6f23be
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-05 02:20:26 +0600

    Update argument.py

--- funtion argument/argument.py
+++ funtion argument/argument.py
@@ -1,1 +1,7 @@
 
+def print_user_info(first_name,last_name):
+    print("User first name is{}".format(first_name))
+    print("User first name is{}".format(last_name))
+    print("User first name is{}".format(first_name,last_name))
+    
+print_user_info(first_name="rakib",last_name="Hassan")
