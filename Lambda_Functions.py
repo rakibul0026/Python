834380a1a279727c446401f5a0c2ee0103054c40
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-07 08:12:26 +0600

    Update Lambda_Functions.py

--- Lambda_Functions.py
+++ Lambda_Functions.py
@@ -1,1 +1,10 @@
+# in Normal Function
+def calculate(a,b):
+ return a*a+2*a*b+b*b
+print("Normal Function")
+print(calculate(2,4))
 
+#Lambda Functions->Lambda Functions is a function  which was no name
+calculate=(lambda a,b:a*a+2*a*b+b*b)(2,3)
+print("Use Lambda Functions")
+print(calculate)
