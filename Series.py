ee0a39472245914716743c07306d212c8bde2805
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-07 11:40:50 +0600

    Update Series.py

--- Series.py
+++ Series.py
@@ -1,1 +1,18 @@
+#1+2+3+........+n
+n=int(input("ENTER  any number"))
+sum=0
+for x in range(1,n+1,1):
+    sum=sum+x
+
+print(sum)
+#1^1+2^2+3^3+........+n
+n=int(input("ENTER  any number"))
+sum=0
+for x in range(1,n+1,1):
+    sum=sum+x*x
+
+print(sum)
+
+
+
 
