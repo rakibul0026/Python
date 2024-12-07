6375671be0b90543115d5e30af1d63f05ce3dc32
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-07 12:03:48 +0600

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,1 +1,11 @@
+from random import randint, random
+for x in range(1, 8):
+        gass_number=int(input("Enter your number:"))
+        ramdom_number=randint(1,100)
+        if gass_number==ramdom_number:
+              print("You are win")
+        else:
+              print("you are lost in the game")
+              print("the random number is:" ,ramdom_number)
+
 
