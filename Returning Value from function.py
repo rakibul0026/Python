1a03967016542ee6dd573f1427be92b898a8339b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-09 01:56:45 +0600

    Update Returning Value from function.py

--- Returning Value from function.py
+++ Returning Value from function.py
@@ -1,1 +1,36 @@
+def add(x, y):
+    sum = x + y
+    return sum
 
+
+def sub(x, y):
+    sub = x - y
+    return sub
+
+
+def multi(x, y):
+    mult = x * y
+    return mult
+
+
+def display():
+    print("Wellcome to CSTU,CSE")
+
+
+
+print(add(2, 9))
+print(add(1, 10))
+print(add(25, 19))
+print(add(23, 90))
+
+print(sub(2, 9))
+print(sub(4, 8))
+print(sub(1, 9))
+print(sub(7, 9))
+
+display()
+
+print(multi(9, 8))
+print(multi(1, 7))
+print(multi(2, 6))
+print(multi(3, 2))
