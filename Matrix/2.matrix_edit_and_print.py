51e647cbed53d7551158afa0a160fb839277350f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-08 08:36:00 +0600

    Update 2.matrix_edit_and_print.py

--- Matrix/2.matrix_edit_and_print.py
+++ Matrix/2.matrix_edit_and_print.py
@@ -1,1 +1,14 @@
+matrix=[
+    [1,2,3],
+    [4,5,6],
 
+]
+#edit matrix
+matrix[0][1]=12
+print(matrix[0][1])
+
+#print matrix using for loop
+print("The Matrix Element Are:")
+for row in matrix:
+    for element in row:
+       print(element)
