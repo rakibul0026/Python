32215a2529cf903bce7202b7f8745e0d442c486d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-09 22:51:08 +0600

    Update 3.Add three lists using map function and lambda.py

--- Map/3.Add three lists using map function and lambda.py
+++ Map/3.Add three lists using map function and lambda.py
@@ -1,1 +1,12 @@
+#Add three lists using map function and lambda
+num1=[1,2,3]
+num2=[2,5,8]
+num3=[9,5,1]
+print("After the operation:")
+print(num1)
+print(num2)
+print(num3)
 
+result=map(lambda x,y,z:x+y+z,num1,num2,num3)
+print("After the operation:")
+print(list(result))
