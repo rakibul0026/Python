e561074b19b0c21d69f47f4f2466f25efde18e8f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-09 22:21:15 +0600

    Update 1. Write a Python program to triple all numbers in a given list of integers.py

--- Map/1. Write a Python program to triple all numbers in a given list of integers.py
+++ Map/1. Write a Python program to triple all numbers in a given list of integers.py
@@ -1,1 +1,6 @@
-
+#1. Write a Python program to triple all numbers in a given list of integers.
+def multi(x):
+    return x+x+x
+nums = [1, 2, 3, 4, 5, 6, 7]
+result=list(map(multi,nums))
+print(result)
