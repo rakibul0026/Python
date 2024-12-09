764a0f8fd5de4a5cfab763a218c9c5d3a9b30cec
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-09 20:45:25 +0600

    Update xx argument problem 1.py

--- x args and xx args/xx_args/xx argument problem 1.py
+++ x args and xx args/xx_args/xx argument problem 1.py
@@ -1,1 +1,12 @@
+#xx_argument
+def student(**infomation):
+          print(infomation)
 
+student(Name="rakibul",id="B210101026",department="cse")
+student(Name="rifat",id="B210101026",department="cse")
+student(Name="shawon",id="B210101026",department="cse")
+student(Name="fouat",id="B210101026",department="cse")
+student(Name="jobayar",id="B210101026",department="cse")
+student(Name="minhad",id="B210101026",department="cse")
+student(Name="ifran",id="B210101026",department="cse")
+student(Name="reshed",id="B210101026",department="cse")
