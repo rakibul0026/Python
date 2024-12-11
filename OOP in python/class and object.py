cabcc72b58886549d8a335a7dfe4b1217e813971
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-11 17:20:34 +0600

    Update class and object.py

--- OOP in python/class and object.py
+++ OOP in python/class and object.py
@@ -1,1 +1,12 @@
+class student:
+     name="Rakib"
+     id="B210101026"
+     department="CSE"
+     cgpa="3.14"
+p1=student()
+print(p1.name)
+print(p1.id)
+print(p1.department)
+print(p1.cgpa)
+
 
