88614d5a5a36ff2a1f92c8f030962ce7f5320bb3
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-11 15:04:40 +0600

    Update Exception Handling (part-1).py

--- Exception Handling (part-1).py
+++ Exception Handling (part-1).py
@@ -1,1 +1,12 @@
+try:
+    list = [20, 0, 30]
+    result = list[0] / list[3]  
+    print(result)
+except ZeroDivisionError:
+    print("Dividing by zero is not possible.")
+except IndexError:
+    print("Index out of range.")
+    print("DONE")
+finally:
+   print("wellcome to CSE Family in CSTU")
 
