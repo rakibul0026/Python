53acf1b3c6337a878d821b1fec266ac953d9a6e2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-11 15:36:27 +0600

    Update Exception Handling (part-2).py

--- Exception Handling (part-2).py
+++ Exception Handling (part-2).py
@@ -1,1 +1,11 @@
+try:
+    num1=int(input("Enter any number "))
+    num2 =int(input("Enter any number "))
+    result=num1/num2
+    print(result)
+except(ValueError,ZeroDivisionError):
+    print("wellcome to CSE Family in CSE")
+
+finally:
+   print("wellcome to CSE Family in CSTU")
 
