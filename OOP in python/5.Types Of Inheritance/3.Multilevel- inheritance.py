5f5d3229dce301ef53ea406ea6ffe1a50d4c39f8
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 21:45:23 +0600

    Update 3.Multilevel- inheritance.py

--- OOP in python/5.Types Of Inheritance/3.Multilevel- inheritance.py
+++ OOP in python/5.Types Of Inheritance/3.Multilevel- inheritance.py
@@ -1,1 +1,18 @@
+class phone:
+    def call(self):
+        print("You can call")
+    def message(self):
+        print("you can message")
+class vivo(phone): # single Inheritance
+    def photo(self):
+        print("You can photo")
+class samsung(vivo): #Multilevel inheritance
+    def google(self):
+        print("you can google search")
 
+
+ob=samsung()
+ob.call()
+ob.photo()
+ob.message()
+ob.google()
