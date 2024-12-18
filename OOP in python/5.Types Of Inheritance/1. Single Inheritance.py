998e9650e9272ccca213c6683e4de5709f6ed4cd
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 21:34:55 +0600

    Update 1. Single Inheritance.py

--- OOP in python/5.Types Of Inheritance/1. Single Inheritance.py
+++ OOP in python/5.Types Of Inheritance/1. Single Inheritance.py
@@ -1,1 +1,14 @@
+class phone:
+    def call(self):
+        print("You can call")
+    def message(self):
+        print("you can message")
+class vivo(phone): #Inheritance
+    def photo(self):
+        print("You can photo")
+
+ob=vivo()
+ob.call()
+ob.photo()
+ob.message()
 
