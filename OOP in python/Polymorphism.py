c327861144b39a3462a97314cdb52bd9621713d1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-19 00:03:14 +0600

    Update Polymorphism.py

--- OOP in python/Polymorphism.py
+++ OOP in python/Polymorphism.py
@@ -1,1 +1,23 @@
+from lamda_function import calculate
+
+
+class A:
+    def print(self):
+        print("Here is A")
+# Run-Time Polymorphism: Method Overriding
+class B(A):
+    def print(self):
+        print("here is B")
+class Calculator:
+    def add(self, a, b=0, c=0):
+        return a + b + c
+
+ob=B()
+ob.print()
+
+# Compile-Time Polymorphism
+ob2=Calculator()
+print(ob2.add(2,3,5))
+print(ob2.add(2,5))
+print(ob2.add(2))
 
