4484da2e3ad3dc590ab2fbe47a9acfb8f8196ce7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 21:49:05 +0600

    Update 2.Multiple inheritance.py

--- OOP in python/5.Types Of Inheritance/2.Multiple inheritance.py
+++ OOP in python/5.Types Of Inheritance/2.Multiple inheritance.py
@@ -1,1 +1,23 @@
- 2. Multiple Inheritance:
+ #Multiple Inheritance
+class phone:
+    def call(self):
+        print("You can call")
+    def message(self):
+        print("you can message")
+class vivo(phone): # single Inheritance
+    def photo(self):
+        print("You can photo")
+class samsung(vivo): #Multilevel inheritance
+    def google(self):
+        print("you can google search")
+class iphone(samsung,vivo): # Multiple Inheritance
+    def video(self):
+        print("you can video in this")
+
+
+ob=iphone()
+ob.call()
+ob.photo()
+ob.message()
+ob.google()
+ob.video()
