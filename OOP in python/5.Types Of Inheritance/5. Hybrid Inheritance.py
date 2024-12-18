de6170da32ed5260e275187251e4c4fd414dfdbb
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 21:56:03 +0600

    Update 5. Hybrid Inheritance.py

--- OOP in python/5.Types Of Inheritance/5. Hybrid Inheritance.py
+++ OOP in python/5.Types Of Inheritance/5. Hybrid Inheritance.py
@@ -1,1 +1,27 @@
+#5. Hybrid Inheritance
+class Phone:
+    def call(self):
+        print("You can call")
+    
+    def message(self):
+        print("You can message")
 
+class Vivo(Phone):  # Single Inheritance
+    def photo(self):
+        print("You can take photos")
+
+class Samsung(Vivo):  # Multilevel Inheritance
+    def google(self):
+        print("You can use Google search")
+
+class iPhone(Samsung):  # Multilevel and Hybrid Inheritance
+    def video(self):
+        print("You can record videos")
+
+# Create an object of the iPhone class
+ob = iPhone()
+ob.call()       # From Phone class
+ob.message()    # From Phone class
+ob.photo()      # From Vivo class
+ob.google()     # From Samsung class
+ob.video()      # From iPhone class
