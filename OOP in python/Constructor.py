84f62e3b6d97680672f5bd9e3eef2608ed7e77f7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 12:52:20 +0600

    Update Constructor.py

--- OOP in python/Constructor.py
+++ OOP in python/Constructor.py
@@ -1,1 +1,17 @@
+class student:
+    def __init__(self,name,id,mark):
+        self.name=name
+        self.id=id
+        self.mark=mark
+    def wellcome(self): #method
+        print("bangladesh")
+    def get_marks(self):
+        return self.mark
+s1=student('rakib',26,87)
+print(s1.name)
+print(s1.id)
+print(s1.mark)
 
+
+s1.wellcome() #call method
+print(s1.get_marks())
