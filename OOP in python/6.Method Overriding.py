1351bd7fff05d962ae5c3662862612310ad9960d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 22:33:22 +0600

    Update 6.Method Overriding.py

--- OOP in python/6.Method Overriding.py
+++ OOP in python/6.Method Overriding.py
@@ -1,1 +1,16 @@
+class phone:
+    def call(self):
+        print("You can call")
+    def message(self):
+        print("you can message")
+class vivo(phone):
+    def call(self):
+        print("this is call section")
+        super().call()
+    def photo(self):
+        print("You can photo")
 
+ob=vivo()
+ob.call()
+ob.photo()
+ob.message()
