2c16d77d0247b6e4689f216416bb5313c0328276
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-18 21:53:44 +0600

    Update 4. Hierarchical Inheritance.py

--- OOP in python/5.Types Of Inheritance/4. Hierarchical Inheritance.py
+++ OOP in python/5.Types Of Inheritance/4. Hierarchical Inheritance.py
@@ -1,1 +1,30 @@
+4. Hierarchical Inheritance
+class Phone:
+    def call(self):
+        print("You can call")
+    def message(self):
+        print("You can message")
 
+class Vivo(Phone):  # Hierarchical inheritance
+    def photo(self):
+        print("You can take photos with Vivo")
+
+class Samsung(Phone):  # Hierarchical inheritance
+    def google(self):
+        print("You can perform Google searches with Samsung")
+
+# Create objects of Vivo and Samsung
+vivo_phone = Vivo()
+samsung_phone = Samsung()
+
+# Vivo phone functionalities
+vivo_phone.call()
+vivo_phone.message()
+vivo_phone.photo()
+
+print()
+
+# Samsung phone functionalities
+samsung_phone.call()
+samsung_phone.message()
+samsung_phone.google()
