22ed078923c4c1eaf5ee521bdda6af29c8787e9b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-12-19 08:06:16 +0600

    Update 8.Abstraction Exercise.py

--- OOP in python/8.Abstraction Exercise.py
+++ OOP in python/8.Abstraction Exercise.py
@@ -1,1 +1,15 @@
+from abc import ABC,abstractmethod
+class shape:
+    def __init__(self,	Radius):
+        self.Radius=Radius
+
+    def calculate_area(self):
+        pass
+class  Circle(shape):
+    def calculate_area(self):
+        area=3.1416*self.Radius
+        print("The area of the circle is:"+str(area))
+Radius=float(input("Enter the Radius :"))
+ob1=Circle(Radius)
+ob1.calculate_area()
 
